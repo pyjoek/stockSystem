f9fb7bad5a1c7abdf763b431d2c52be1780ce5a1
pyyoek <joelnrlson@gmai.com>
2024-11-24 08:33:35 +0300

    no idea whats going on

--- resources/views/vita1/vita1Store.blade.php
+++ resources/views/vita1/vita1Store.blade.php
@@ -40,6 +40,8 @@
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h2>Stock Information</h2>
             <a href="#" class="btn btn-primary">Request Stock</a>
+            <a href="/logout" class="btn btn-primary">Logout</a>
+
         </div>
         <h3 class="text-muted">Vita Store Branch 1</h3>
         <table class="table table-bordered table-hover">
